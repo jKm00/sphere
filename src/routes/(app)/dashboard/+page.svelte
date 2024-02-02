f131fe0470de0f870bcfb219a312ca3c3a3b81e8
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-02-03 00:27:30 +0100

    Email verification

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import { session } from '$lib/session';
+	import VerifyEmail from './VerifyEmail.svelte';
 </script>
 
-<h1>Dashboard: {$session?.user?.email}</h1>
-<a href="/logout">Logout</a>
+<main class="width m-auto p-4">
+	{#if !$session?.user?.emailVerified}
+		<VerifyEmail />
+	{/if}
+	<h1>Dashboard: {$session?.user?.email}</h1>
+	<a href="/logout">Logout</a>
+</main>
+
+<style>
+	.width {
+		width: min(100%, 800px);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { Toaster } from '$lib/components/ui/sonner';
 	import '../app.pcss';
 </script>
+
+<Toaster />
 
 <slot />
