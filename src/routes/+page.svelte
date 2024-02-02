7b73728501c9983211b662c123b540c695f6b3a2
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-02-02 18:21:43 +0100

    Landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { Mail } from 'lucide-svelte';
+</script>
+
+<main class="flex h-screen items-center justify-center">
+	<div class="grid gap-8 text-center">
+		<div>
+			<h1 class="mb-2 text-6xl font-bold uppercase">Sphere</h1>
+			<p class="text-sm">Keep track of all your subscriptions</p>
+		</div>
+		<div class="flex items-center justify-center gap-4">
+			<span class="h-[1px] flex-grow rounded bg-primary"></span>
+			<h2>Login</h2>
+			<span class="h-[1px] flex-grow bg-primary"></span>
+		</div>
+		<div class="grid gap-2">
+			<Button class="flex justify-between" variant="outline">
+				<img src="/icons/google.png" alt="Google icon" class="h-full" />
+				Google
+				<img src="/icons/google.png" alt="Google icon" class="h-full opacity-0" />
+			</Button>
+			<Button href="/login" class="flex justify-between" variant="outline">
+				<Mail class="h-[80%]" />
+				Email
+				<Mail class="h-[80%] opacity-0" />
+			</Button>
+		</div>
+	</div>
+</main>
