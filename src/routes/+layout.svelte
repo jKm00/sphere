672c15c3785c64a0fa85ca85a51b65e07c7ba561
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-02-07 20:07:50 +0100

    fix: light/dark theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
 	import { Toaster } from '$lib/components/ui/sonner';
 	import '../app.pcss';
 	import ThemeToggler from '$lib/components/ThemeToggler.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
+<ModeWatcher />
 <Toaster />
 
 <div class="absolute right-4 top-4 flex items-center gap-4">
