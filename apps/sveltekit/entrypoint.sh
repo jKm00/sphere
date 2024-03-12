0609edda632bbd861bc84f4f4e1f3af508b80504
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-03-12 20:05:12 +0100

    feat: docker compose with nginx proxy

--- apps/sveltekit/entrypoint.sh
+++ apps/sveltekit/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-# entrypoint.sh
 
-# Run prisma migrations
 npx prisma migrate deploy
 
-# Start application
-ORIGIN="${ORIGIN}" node -r dotenv/config build
+ORIGIN="${ORIGIN}" node build
